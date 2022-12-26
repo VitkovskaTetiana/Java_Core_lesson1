8dd92ce9108861a1d3ba6cc69bfed8b556550074
Tetiana Vitkovska <taniavitkovskaia@gmail.com>
2022-12-26 23:32:03 +0100

    commit2

--- Lesson1.java
+++ Lesson1.java
@@ -14,7 +14,6 @@ public class Lesson1 {
 		char j = 'j';
 
 		///////////////////////////////////////
-		///////////////////////////////////////
 
 		System.out.println("max value for byte is " + Byte.MAX_VALUE);
 		System.out.println("min value for byte is " + Byte.MIN_VALUE);
